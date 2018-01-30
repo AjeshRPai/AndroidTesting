c37381ec015451dbe4ce002b84dcb860487a31ae
Ajesh R Pai <ajeshr2k1@gmail.com>
2018-01-30 14:28:22 +0530

    Final Checks and Test functions modified

--- app/src/androidTest/java/com/example/ajeshpai/androidtesting/MainActivityInstrumentationTest.kt
+++ app/src/androidTest/java/com/example/ajeshpai/androidtesting/MainActivityInstrumentationTest.kt
@@ -2,19 +2,12 @@ package com.example.ajeshpai.androidtesting
 
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import org.hamcrest.Matchers
 import org.junit.runner.RunWith
-import java.util.regex.Matcher
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewAction
 import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.util.Log
-import android.view.View
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.view.*
-import org.hamcrest.Matchers.allOf
 import org.junit.*
 
 
@@ -24,21 +17,18 @@ class MainActivityInstrumentationTest {
     //rule is initialized
     @Rule
     @JvmField
-    public val rule  = ActivityTestRule(MainActivity::class.java,false,false)
-
-    private val resources by lazy {
-        rule.activity.resources
-    }
+    public val rule  = getRule()
 
 
     private val username_tobe_typed="Ajesh"
-    private val password_tobe_typed="password"
+    private val correct_password ="password"
+    private val wrong_password = "passme123"
 
 
 
     private fun getRule(): ActivityTestRule<MainActivity> {
         Log.e("Initalising rule","getting Mainactivity")
-        return ActivityTestRule(MainActivity::class.java,false,false)
+        return ActivityTestRule(MainActivity::class.java)
     }
 
 
@@ -61,34 +51,41 @@ class MainActivityInstrumentationTest {
     @Before
     fun before_test_method(){
         Log.e("@Before","Hi this is run before every test function")
-        //Lets check if the hints are correct on edit texts
     }
 
 
     @Test
-    fun login_valid(){
-        Log.e("@Test","Performing login valid test")
+    fun login_success(){
+        Log.e("@Test","Performing login success test")
         Espresso.onView((withId(R.id.user_name)))
                 .perform(ViewActions.typeText(username_tobe_typed))
-                .check(matches(withText("Ajesh")))
 
         Espresso.onView(withId(R.id.password))
-                .perform(ViewActions.typeText(password_tobe_typed))
-                .check(matches(withText("password")))
+                .perform(ViewActions.typeText(correct_password))
 
         Espresso.onView(withId(R.id.login_button))
                 .perform(ViewActions.click())
+
+        Espresso.onView(withId(R.id.login_result))
+                .check(matches(withText(R.string.login_success)))
+
+
     }
 
     @Test
-    fun login_Hint(){
-        Log.e("@Test","Performing login hint test")
-        Espresso.onView(withId(R.id.user_name))
-                .check(matches(withHint(resources.getString(R.string.username_hint))))
+    fun login_failure(){
+        Log.e("@Test","Performing login failure test")
+        Espresso.onView((withId(R.id.user_name)))
+                .perform(ViewActions.typeText(username_tobe_typed))
+
         Espresso.onView(withId(R.id.password))
-                .check(matches(withHint(resources.getString(R.string.password_hint))))
+                .perform(ViewActions.typeText(wrong_password))
+
         Espresso.onView(withId(R.id.login_button))
-                .check(matches(withText(resources.getString(R.string.login_button))))
+                .perform(ViewActions.click())
+
+        Espresso.onView(withId(R.id.login_result))
+                .check(matches(withText(R.string.login_failed)))
 
     }
 
--- app/src/main/java/com/example/ajeshpai/androidtesting/MainActivity.kt
+++ app/src/main/java/com/example/ajeshpai/androidtesting/MainActivity.kt
@@ -4,17 +4,39 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private val login_button by lazy{
+    private val button_login by lazy{
         findViewById<Button>(R.id.login_button)
+    }
+
+    private val editText_username by lazy {
+        findViewById<EditText>(R.id.user_name)
+    }
+
+    private val editText_password by lazy {
+        findViewById<EditText>(R.id.password)
+    }
+
+    private val textView_result by lazy {
+        findViewById<TextView>(R.id.login_result)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.e("Activity","onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_login.setOnClickListener{
+            if(editText_username.text.toString().equals("Ajesh") &&
+                    editText_password.text.toString().equals("password"))
+                textView_result.text=getString(R.string.login_success)
+            else
+                textView_result.text=getString(R.string.login_failed)
+        }
 
     }
 
